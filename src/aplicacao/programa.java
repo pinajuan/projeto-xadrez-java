6daa95db10ac23c502a30da26dfdb8ec7e6a89d9
Juan Pina <juanpinaribeiro@outlook.com>
2023-06-17 19:37:17 -0300

    Criação do Projeto Xadrez em Java

---
+++ src/aplicacao/programa.java
@@ -0,0 +1,9 @@
+package aplicacao;
+
+public class programa {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Pina
+ *
+ */
+module ProjetoXadrez {
+}
